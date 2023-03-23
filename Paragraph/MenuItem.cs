e3fa8aba54197c4d3d2ce77732df5b82595f3cfd
Egor Fedorenko <egorfedorenko.w@gmail.com>
2023-03-23 17:12:38 +0300

    remove setters

--- Paragraph/MenuItem.cs
+++ Paragraph/MenuItem.cs
@@ -2,8 +2,9 @@ namespace Paragraph;
 
 public class MenuItem
 {
-    public string Name { get; set; }
-    public double Price { get; set; }
+    public string Name { get; }
+
+    public double Price { get; }
 
     public MenuItem(string name, double price)
     {
